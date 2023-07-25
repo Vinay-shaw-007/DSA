ed6d33214e749ce4c921cb05f461ad60899f30fa
Vinay Shaw <abhishaw5339@gmail.com>
2023-07-25 15:29:42 +0530

    Bit manipulation -> Non repeating numbers

---
+++ src/Bit_Manipulation/Non_Repeating_Numbers.java
@@ -0,0 +1,43 @@
+package Bit_Manipulation;
+
+public class Non_Repeating_Numbers {
+
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 13, 7, 23, 24, 13, 26, 23, 24, 16, 7, 19, 3, 26, 17};
+        singleNumber(arr);
+    }
+
+    private static void singleNumber(int[] nums) {
+        int[] ans = new int[2];
+        int res = 0, sitBitIndex = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+
+        String resBinary = Integer.toBinaryString(res);
+
+        for (int i = resBinary.length() - 1; i >= 0; i--) {
+            char ch = resBinary.charAt(i);
+            if (ch == '1') {
+                sitBitIndex = resBinary.length() - i;
+                break;
+            }
+        }
+        sitBitIndex--;
+        sitBitIndex = (int) Math.pow(2, sitBitIndex);
+
+        for (int num : nums) {
+            if ((num & sitBitIndex) == 0) ans[0] ^= num;
+            else ans[1] ^= num;
+        }
+
+        if (ans[0] > ans[1]) {
+            int temp = ans[0];
+            ans[0] = ans[1];
+            ans[1] = temp;
+        }
+        System.out.println(ans[0]);
+        System.out.println(ans[1]);
+    }
+}
