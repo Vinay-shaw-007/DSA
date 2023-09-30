21be29b82528ae2c7baa464575964ed371d07f50
Vinay <abhishaw5339@gmail.com>
2023-09-30 10:48:34 +0530

    LinkedList -> Rotate_List

--- src/LinkedList/Rotate_List.java
+++ src/LinkedList/Rotate_List.java
@@ -41,15 +41,15 @@ public class Rotate_List {
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-//        n5.next = n6;
-//        n6.next = n7;
-//        n7.next = n8;
-//        n8.next = n9;
-//        n9.next = n10;
+        n5.next = n6;
+        n6.next = n7;
+        n7.next = n8;
+        n8.next = n9;
+        n9.next = n10;
 
 
         printLL(head);
-        printLL(rotateRight(head, 10));
+        printLL(rotateRight(head, 276));
     }
 
     public static ListNode rotateRight(ListNode A, int B) {
@@ -68,24 +68,14 @@ public class Rotate_List {
 
         int travel = B;
 
-
-        if (travel > size) {
-            while (travel > size) {
-                travel = travel - size;
-            }
-            System.out.println("while travel = " + travel);
+        if (travel % size == 0) {
+            travel = travel % size;
+        } else {
+            travel = travel % size;
+            travel = size - travel;
         }
 
-        travel = Math.abs(travel);
-
-        travel = size - travel;
-
-
-        System.out.println("travel = " + travel);
-
-
         if (travel == 0) return A;
-
 
         int i = 1;
         temp = A;
@@ -115,31 +105,4 @@ public class Rotate_List {
         return newHead;
 
     }
-//    public static ListNode rotateRight(ListNode A, int B) {
-//        //You can code here
-//
-//        if (A == null || A.next == null) return A;
-//        ListNode ans = new ListNode(-1);
-//
-//        int size = 0;
-//
-//        ListNode temp = A;
-//
-//        while (temp != null) {
-//            size++;
-//            temp = temp.next;
-//        }
-//
-//        temp = A;
-//
-//        while (temp.next != null) {
-//            temp = temp.next;
-//        }
-//
-//        temp.next = A;
-//
-//
-//        return ans.next;
-//
-//    }
 }
